51a53f01f9abe665b1bfefbf1d832ab681602a3d
derfred3 <Der-Fred3@web.de>
2025-11-04 13:31:00 +0100

    Add comments for readability of bash

--- create_zip.sh
+++ create_zip.sh
@@ -16,12 +16,14 @@ usage() {
         exit 1
 }
 
+# utility for logging
 log() {
         if [ ${SILENT} -eq 0 ]; then
                 echo "$1"
         fi
 }
 
+# read call options
 while getopts ":rns" o; do
         case "${o}" in
                 r)
@@ -42,11 +44,13 @@ done
 config_name=".zip_config"
 lines_read=0
 zip_name=""
-
 full_name=""
+
+# read name-config file
 while read line; do
         lines_read=$(($lines_read+1))
         if [ ${lines_read} -eq 1 ]; then
+                # ignore first line
                 continue
         elif [ ${lines_read} -eq 2 ]; then
                 # split line and fill zip_name in correct format
@@ -55,10 +59,12 @@ while read line; do
                         zip_name="${zip_name}_${name}"
                 done
         else
-                exit 1
+                # only accept second line
+                break
         fi
 done < "${PWD}/${config_name}"
 
+# check if actual content was read
 if [ -z ${zip_name} ]; then
         echo "Could not find a name within file <${PWD}/${config_name}>" 1>&2; exit 1;
 fi
@@ -68,25 +74,31 @@ log "Found name <${full_name}> in file <${PWD}/${config_name}>"
 log "Processing folders.."
 log ""
 
+# continue iterating over task folders
 for dir in [0-9][0-9]* ; do
         full_path="${PWD}/${dir}"
 
         log "  ${full_path}:"
 
         files_cfg="${full_path}/${config_name}"
+
+        # check if zip-config exists within task folder
         if ! test -f ${files_cfg}; then
                 log "    - No config file found in here, continuing with next folder.."
                 log ""
                 continue
         fi
 
-        # config file exists
         lines_read=0
+
+        # read zip-config of task folder
         while read line; do
                 lines_read=$(($lines_read+1))
                 if [ ${lines_read} -eq 1 ]; then
+                        # ignore first line
                         continue
                 elif [ ${lines_read} -eq 2 ]; then
+                        # trim leading 0s
                         if [ ${dir:0:1} -eq 0 ]; then
                                 zip_name_full="a${dir:1:1}${zip_name}.zip"
                         else
@@ -99,6 +111,8 @@ for dir in [0-9][0-9]* ; do
                         # Only create zip, if not a dry run and file does not exist, or REPLACE flag is set
                         if [ ${DRY_RUN} -eq 0 ] && [ ! -f ${full_path}/${zip_name_full} ] || [ ${REPLACE} -eq 1 ]; then
                                 log "    - Trying to execute: zip ${full_path}/${zip_name_full} ${full_path}/${line}" 
+
+                                # actual command execution using zip
                                 zip ${full_path}/${zip_name_full} ${full_path}/${line}
                         else
                                 if [ ${DRY_RUN} -eq 1 ]; then
@@ -110,6 +124,7 @@ for dir in [0-9][0-9]* ; do
                                 fi
                         fi
                 else
+                        # only accept second line
                         break
                 fi
         done < ${files_cfg}
