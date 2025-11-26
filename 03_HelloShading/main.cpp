212a079a700fe2d6d75723d6d1a673c6f4978a47
arturo-olvrs <2004.olivares@gmail.com>
2025-11-26 01:51:00 +0100

    A3 Exercises 1 y 2 done

--- 03_HelloShading/main.cpp
+++ 03_HelloShading/main.cpp
@@ -4,15 +4,19 @@
 #include <Vec2.h>
 #include "Teapot.h"
 #include "UnitPlane.h"
+#include "UnitCube.h"
 
 class MyGLApp : public GLApp {
 public:
   double time{};
+  const float degreePreSecond{ 45.0f };
 
   GLuint pFlat{};
   GLuint pGouraud{};
   GLuint pPhong{};
+  GLuint pLight{};
   Mat4 modelMatrix{};
+  Mat4 animatedModelMatrix{};
   Mat4 projectionMatrix{};
   Mat4 viewMatrix{};
   bool leftMouseDown{false};
@@ -20,25 +24,37 @@ public:
   bool controlDown{false};
 
   GLint modelViewProjectionMatrixUniform{-1};
+  GLint modelViewUniform{-1};
+  GLint normalMatrixUniform{-1};
+  GLint lightPosUniform{-1};
+  GLint viewPosUniform{-1};
+  
 
   enum class Shading {
     FLAT, GOURAUD, PHONG
   };
 
-  Shading shading{Shading::FLAT};
+  Shading initialShading{Shading::FLAT};
+  Shading shading{};
 
   Vec3 kd = { 1, 1, 1 }; // material diffuse color
   GLint kdUniform{-1};
 
-  GLuint vbos[4]{ };
-  GLuint vaos[2]{ };
+  GLuint vbos[6]{ };
+  GLuint vaos[3]{ };
   GLuint eboTeapot{};
+  GLuint eboCube{};
+
+  // Light
+  Vec3 lightPosition = { -35.0f, 35.0f, 35.0f };  // initial light position
 
   // camera
   bool cameraActive{false};
   bool firstCameraUpdate{true};
-  Vec3 viewPosition = { 0, 0, -100 }; // view translation position
-  Vec3 viewRotation = { -45, 0, 0 }; // view rotation angles
+  Vec3 initialViewPosition = { 0, 0, -100 }; // view translation position
+  Vec3 initialViewRotation = { -45, 0, 0 }; // view rotation angles
+  Vec3 viewPosition;
+  Vec3 viewRotation;
   Vec2 mouse = { 0, 0 }; // last mouse position
   float mouseSensitivity{0.15f}; // system specific factor
   float mousewheelFactor{10.0f}; // system specific factor
@@ -50,6 +66,14 @@ public:
     setupShaders();
     setupGeometry();
     GL(glClearColor(0.0f, 0.0f, 0.0f, 1.0f));
+
+    resetAnimation();
+    setAnimation(false);
+    shading = initialShading;
+    viewPosition = initialViewPosition;
+    viewRotation = initialViewRotation;
+
+    GL(glEnable(GL_DEPTH_TEST));
   }
 
   void selectShading() {
@@ -72,6 +96,12 @@ public:
     }
   }
 
+  virtual void animate(double animationTime) override {
+    double angle = animationTime * degreePreSecond;
+    
+    animatedModelMatrix = Mat4::rotationY(angle);
+  }
+
   virtual void draw() override {
     double t = glfwGetTime();
     double d = t - time;
@@ -79,24 +109,59 @@ public:
 
     GL(glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT));
 
-    selectShading();
     viewMatrix = Mat4::translation(viewPosition[0], viewPosition[1], viewPosition[2]);
     viewMatrix = viewMatrix * Mat4::rotationX(viewRotation[0]);
     viewMatrix = viewMatrix * Mat4::rotationY(viewRotation[1]);
     viewMatrix = viewMatrix * Mat4::rotationZ(viewRotation[2]);
 
+
+    // Draw light source
+    GL(glUseProgram(pLight));
+
+    modelMatrix = animatedModelMatrix *
+                  Mat4::translation(lightPosition[0], lightPosition[1], lightPosition[2]) *
+                  Mat4::scaling(1.0f, 1.0f, 1.0f);
+
+    Mat4 modelView = viewMatrix * modelMatrix;
+    Mat4 modelViewProjection = projectionMatrix * modelView;
+
+    GL(glUniformMatrix4fv(modelViewProjectionMatrixUniform, 1, GL_TRUE, modelViewProjection));
+    GL(glBindVertexArray(vaos[2]));
+    GL(glDrawElements(GL_TRIANGLES, sizeof(UnitCube::indices) / sizeof(UnitCube::indices[0]), GL_UNSIGNED_INT, (void*)0));
+
+
+    // Prepare shaders
+    selectShading();
+    GL(glUniform3fv(lightPosUniform, 1, modelView * lightPosition));
+
+    // draw unit plane
     modelMatrix = Mat4();
     modelMatrix = modelMatrix * Mat4::scaling(100, 100, 100);
-    Mat4 modelViewProjection = projectionMatrix * viewMatrix * modelMatrix;
+    modelView = viewMatrix * modelMatrix;
+    modelViewProjection = projectionMatrix * modelView;
+    Mat4 normalMatrix = Mat4::transpose(Mat4::inverse(modelView));
+
+    GL(glUniformMatrix4fv(modelViewUniform, 1, GL_TRUE, modelView));
+    GL(glUniformMatrix3fv(normalMatrixUniform, 1, GL_TRUE, normalMatrix));
     GL(glUniformMatrix4fv(modelViewProjectionMatrixUniform, 1, GL_TRUE, modelViewProjection));
     GL(glBindVertexArray(vaos[0]));
     GL(glDrawArrays(GL_TRIANGLES, 0, sizeof(UnitPlane::vertices) / (3*sizeof(UnitPlane::vertices[0]))));
 
+
+    // draw teapot
     modelMatrix = Mat4();
-    modelViewProjection = projectionMatrix * viewMatrix * modelMatrix;
+    modelView = viewMatrix * modelMatrix;
+    modelViewProjection = projectionMatrix * modelView;
+    normalMatrix = Mat4::transpose(Mat4::inverse(modelView));
+
+
+    GL(glUniformMatrix4fv(modelViewUniform, 1, GL_TRUE, modelView));
+    GL(glUniformMatrix3fv(normalMatrixUniform, 1, GL_TRUE, normalMatrix));
     GL(glUniformMatrix4fv(modelViewProjectionMatrixUniform, 1, GL_TRUE, modelViewProjection));
     GL(glBindVertexArray(vaos[1]));
     GL(glDrawElements(GL_TRIANGLES, sizeof(Teapot::indices) / sizeof(Teapot::indices[0]), GL_UNSIGNED_INT, (void*)0));
+
+
     GL(glBindVertexArray(0));
 
   }
@@ -142,7 +207,12 @@ public:
     checkAndThrowProgram(pFlat);
 
     modelViewProjectionMatrixUniform = glGetUniformLocation(pFlat, "MVP");
-    kdUniform = glGetUniformLocation(pFlat, "kd");
+    kdUniform = glGetUniformLocation(pFlat, "Kd");
+    modelViewUniform = glGetUniformLocation(pFlat, "MV");
+    normalMatrixUniform = glGetUniformLocation(pFlat, "NormalMat");
+    lightPosUniform = glGetUniformLocation(pFlat, "lightPos");
+    viewPosUniform = glGetUniformLocation(pFlat, "viewPos");
+
 
     GL(glDeleteShader(vertexShader));
     GL(glDeleteShader(fragmentShader));
@@ -172,15 +242,28 @@ public:
     GL(glDeleteShader(fragmentShader));
 
     selectShading();
+
+    vertexSrcPath =  "res/shaders/light.vert";
+    fragmentSrcPath =  "res/shaders/light.frag";
+    vertexShader = createShaderFromFile(GL_VERTEX_SHADER, vertexSrcPath);
+    fragmentShader = createShaderFromFile(GL_FRAGMENT_SHADER, fragmentSrcPath);
+    pLight = glCreateProgram();
+    GL(glAttachShader(pLight, vertexShader));
+    GL(glAttachShader(pLight, fragmentShader));
+    GL(glLinkProgram(pLight));
+    checkAndThrowProgram(pLight);
+    GL(glDeleteShader(vertexShader));
+    GL(glDeleteShader(fragmentShader));
   }
 
   void setupGeometry() {
     const GLuint vertexPositionLocation = GLuint(glGetAttribLocation(pPhong, "vertexPosition"));
     const GLuint vertexNormalLocation = GLuint(glGetAttribLocation(pPhong, "vertexNormal"));
 
-    GL(glGenVertexArrays(2, vaos));
-    GL(glGenBuffers(4, vbos));
+    GL(glGenVertexArrays(3, vaos));
+    GL(glGenBuffers(6, vbos));
     GL(glGenBuffers(1, &eboTeapot));
+    GL(glGenBuffers(1, &eboCube));
 
     // unit plane in xy-plane
     GL(glBindVertexArray(vaos[0]));
@@ -211,6 +294,23 @@ public:
     GL(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, eboTeapot));
     GL(glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Teapot::indices), Teapot::indices, GL_STATIC_DRAW));
 
+    // unit cube
+    GL(glBindVertexArray(vaos[2]));
+    // setup vertices
+    GL(glBindBuffer(GL_ARRAY_BUFFER, vbos[4]));
+    GL(glBufferData(GL_ARRAY_BUFFER, sizeof(UnitCube::vertices), UnitCube::vertices, GL_STATIC_DRAW));
+    GL(glVertexAttribPointer(vertexPositionLocation, 3, GL_FLOAT, GL_FALSE, 0, (void*)0));
+    GL(glEnableVertexAttribArray(vertexPositionLocation));
+    // setup normals
+    GL(glBindBuffer(GL_ARRAY_BUFFER, vbos[5]));
+    GL(glBufferData(GL_ARRAY_BUFFER, sizeof(UnitCube::normals), UnitCube::normals, GL_STATIC_DRAW));
+    GL(glVertexAttribPointer(vertexNormalLocation, 3, GL_FLOAT, GL_FALSE, 0, (void*)0));
+    GL(glEnableVertexAttribArray(vertexNormalLocation));
+    // setup indices for indexed drawing the unit cube
+    GL(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, eboCube));
+    GL(glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(UnitCube::indices), UnitCube::indices, GL_STATIC_DRAW));
+
+
     GL(glBindVertexArray(0));
   }
 
@@ -233,6 +333,17 @@ public:
         case GLENV_KEY_P:
           shading = Shading::PHONG;
           std::cout << "switched to PHONG shading" << std::endl;
+          break;
+        case GLENV_KEY_R:
+          resetAnimation();
+          shading = initialShading;
+          viewPosition = initialViewPosition;
+          viewRotation = initialViewRotation;
+          std::cout << "animation and interaction reset" << std::endl;
+          break;
+        case GLENV_KEY_SPACE:
+          setAnimation(!getAnimation());
+          std::cout << "animation " << (getAnimation() ? "started" : "stopped") << std::endl;
           break;
       }
 
--- 03_HelloShading/res/shaders/flat.vert
+++ 03_HelloShading/res/shaders/flat.vert
@@ -1,16 +1,61 @@
 #version 410 core
 
-layout(location = 0) in vec3 vertexPosition;  // vertex position in object/model space
-layout(location = 1) in vec3 vertexNormal;    // vertex normal in object/model space
+// ----------- Inputs from VAO --------------
+layout(location = 0) in vec3 vertexPosition;
+layout(location = 1) in vec3 vertexNormal;
 
-uniform mat4 MVP; // model-view-projection Matrix
+// ----------- Uniforms --------------
+uniform mat4 MVP;        // Model-View-Projection matrix
+uniform mat4 MV;         // Model-View matrix
+uniform mat3 NormalMat;  // Normal matrix
 
-uniform vec3 kd; // material diffuse color
+uniform vec3 lightPos;   // Position of point light in world space
+uniform vec3 viewPos;    // Position of camera
 
-out vec4 C;
+// Material parameters
+uniform vec3 Kd;  // Diffuse color (set in your C++ via kdUniform)
+
+// Output to fragment shader (FLAT!)
+flat out vec4 fragColor;
 
 void main()
 {
-	gl_Position = MVP * vec4(vertexPosition, 1);
-	C = vec4(0.5 * vertexNormal + 0.5 * kd, 1);
+    // Transform vertex
+    gl_Position = MVP * vec4(vertexPosition, 1.0);
+
+    // ----- Compute vectors for lighting -----
+
+    // Position in Eye space
+    vec3 P = vec3(MV * vec4(vertexPosition, 1.0));
+
+    // Correctly transformed normal
+    vec3 N = normalize(NormalMat * vertexNormal);
+
+    // Light direction
+    vec3 L = normalize(lightPos - P);
+
+    // View direction
+    vec3 V = normalize(- P);    // camera at origin
+
+    // Reflection vector
+    vec3 R = reflect(-L, N);
+
+    // ----- Light coefficients -----
+    vec3 LightAmbient  = vec3(0.9);
+    vec3 LightDiffuse  = vec3(0.9);
+    vec3 LightSpecular = vec3(0.9);
+
+    // ----- Material coefficients -----
+    vec3 Ka = vec3(0.05);  // ambient
+    vec3 Ks = vec3(1.0);   // specular
+    float shininess = 50.0;
+
+    // ----- Phong illumination (computed per VERTEX) -----
+
+    vec3 ambient = LightAmbient * Ka;
+    vec3 diffuse = LightDiffuse * Kd * max(dot(N, L), 0.0);
+    vec3 specular = LightSpecular * Ks * pow(max(dot(V, R), 0.0), shininess);
+
+    // Final color
+    fragColor = vec4(ambient + diffuse + specular,1);
 }
